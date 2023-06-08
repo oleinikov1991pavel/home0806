766379dd686c4f7fc1371c51ddcd48920a3acd9f
oleinikov1991pavel <134002094+oleinikov1991pavel@users.noreply.github.com>
2023-06-08 14:27:22 +0300

    123

---
+++ main.py
@@ -0,0 +1,37 @@
+#9.141
+# someString = 'hello4wor5ld6'
+# string = someString.isdigit()
+# num = ''
+#
+# for c in someString:
+#     if c.isdigit():
+#         num += c
+#
+# print('цифры в строке: ' + num)
+
+
+
+
+#161
+# someString = 'сколько дней в году'
+# x = someString.lower().split()
+#
+# result = []
+# for a in x:
+#     for c in a:
+#         if someString.count(c) == a.count(a):
+#             result.append(c)
+#
+# print(result)
+
+#9.166
+# someString = 'я шел по дороге'.split()
+# someString[0], someString[-1] = someString[-1], someString[0]
+#
+# print(someString)
+
+
+
+
+
+
