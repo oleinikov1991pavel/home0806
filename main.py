1d83e2a5dd0485095349a9004644e04483e7ddc9
oleinikov1991pavel <134002094+oleinikov1991pavel@users.noreply.github.com>
2023-06-12 11:07:38 +0300

    1234

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random  #######################################
+                                 # МЕТОДЫ!!!!
+
 #9.141
 # someString = 'hello4wor5ld6'
 # string = someString.isdigit()
@@ -32,6 +35,62 @@
 
 
 
+# 9.113
+
+# someString = 'hellco wocrlcd'.replace('c', '')
+# print(someString)
+
+#9.82
+
+# someString = input('')
+# result = someString[: someString.index(' ')].count('o')
+# print(result)
+
+#9.62
+
+# someString = input('')
+# result = someString.count('a') / len(someString) - someString.count(' ') * 100
+# print(result)
 
 
+# 9.72
 
+# someString = input()
+# result = someString.count(' ') + 1 > 3
+# print(result)
+
+#9.75
+#
+# someString = input()
+# result = someString[: someString.index(',')]
+# print(result)
+
+# анаграмма
+#
+# someString1 = input()
+# someString2 = input()
+#
+# result = len(someString2) == len(someString1)
+#
+# if result:
+#     len1 = len(someString1)
+#     index = 0
+#     while index < len1:
+#         if someString1.count(someString1[index]) != someString2.count(someString1[index]):
+#             result = False
+#             print(result)
+#             break
+#         index += 1
+#
+#     print(result)
+#
+# else:
+#     print(result)
+
+
+#############
+
+someString = '123456787'
+number = 9
+
+summa = 7
